2f9dfb6c438650843cb86d0f816707e4fe0825a8
ArifAlan <91489739+ArifAlan@users.noreply.github.com>
2024-07-09 16:51:52 +0300

    Add project files.

---
+++ NetWithElasticSearch/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetWithElasticSearch.Context;
+
+public sealed class AppDbContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TestDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+    public DbSet<Travel> Travels { get; set; }
+}
+public sealed class Travel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
